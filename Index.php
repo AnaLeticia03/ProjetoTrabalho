1400ab157584e42acd26a0f9609eb2c159a62fe1
Ana leticia <leticia74603895@gmail.com>
2022-03-13 18:51:45 -0300

    Estilização do primeiro bloco

--- Index.php
+++ Index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./Styles/geral.css">
+    <link rel="stylesheet" href="./Styles/index.css">
     <title>CADASTRO</title>
 </head>
 <body>
@@ -28,6 +29,47 @@
         <a class="btn" href="#"> Cadastre-se</a>
 
     </header>
+
+    <main>
+        <section id="bloco1" class="bloco">
+            <div class="coluna" id="esquerda">
+                <div id="titulo">
+                  <h1>Psicologia e tecnologia</h1>
+                </div>
+                <div id="cta">
+                    <div class="conteudo">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dolore, quisquam perferendis explicabo praesentium illo omnis rerum doloremque repellendus est nemo, nihil modi nulla provident nam quasi possimus? Doloribus, velit?</p>
+                        <br>
+                        <a href="#" class="btn">Saiba mais</a>
+                    </div>
+                </div>
+
+            </div>
+            <div class="coluna" id="direita">
+
+            </div>
+        </section>
+
+        <section id="bloco2" class="bloco">
+            2
+        </section>
+            
+        <section id="bloco3" class="bloco">
+            3
+        </section>
+            
+        <section id="bloco4" class="bloco">
+            4
+        </section>
+
+        <section id="bloco5" class="bloco">
+            5
+        </section>
+
+        <section id="bloco6" class="bloco">
+            6
+        </section>
+    </main>
     
 </body>
 </html>
